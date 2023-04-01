2f7758077098e08beef432973f6d918318573ea6
j1nt0 <sk2018univ@naver.com>
2023-04-01 22:30:53 +0900

    2023-04-01

---
+++ Level 2/2. Detail/ProgressView/ProgressView/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  ProgressView
+//
+//  Created by 이진 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Level 2/2. Detail/ProgressView/ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by 이진 on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
