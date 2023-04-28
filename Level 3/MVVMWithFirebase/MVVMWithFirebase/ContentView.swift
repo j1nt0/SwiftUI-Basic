b984145f1fd026ce3531b1f1b8fd40efa54bdeb2
j1nt0 <sk2018univ@naver.com>
2023-04-28 22:14:54 +0900

    2023-04-28

--- Level 3/MVVMWithFirebase/MVVMWithFirebase/ContentView.swift
+++ Level 3/MVVMWithFirebase/MVVMWithFirebase/ContentView.swift
@@ -6,36 +6,7 @@
 //
 
 import SwiftUI
-import Firebase
 
-struct ContentView: View {
-    @State private var documents: [DocumentSnapshot] = []
-
-    var body: some View {
-        List(documents, id: \.documentID) { document in
-            HStack {
-                Text(document.data()?["name"] as? String ?? "")
-                    .font(.system(size: 20))
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text(document.data()?["category"] as? String ?? "")
-                    Text(document.data()?["address"] as? String ?? "")
-                        .font(.system(size: 15))
-                }
-            }
-        }
-        .onAppear {
-            let db = Firestore.firestore()
-            db.collection("shop").getDocuments { querySnapshot, error in
-                if let error = error {
-                    print("Error getting documents: \(error.localizedDescription)")
-                } else {
-                    self.documents = querySnapshot?.documents ?? []
-                }
-            }
-        }
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
