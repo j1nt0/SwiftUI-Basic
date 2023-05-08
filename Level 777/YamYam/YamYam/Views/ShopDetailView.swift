ff4c321bcb0c04758c95dd22df7d1168f2c31249
j1nt0 <sk2018univ@naver.com>
2023-05-09 00:45:25 +0900

    2023-05-09

--- Level 777/YamYam/YamYam/Views/ShopDetailView.swift
+++ Level 777/YamYam/YamYam/Views/ShopDetailView.swift
@@ -11,10 +11,16 @@ struct ShopDetailView: View {
     
     var model = Store(id: "", name: "", number: "", address: "", category: "", menu: ["":0], operatingHour: ["":""], position: [0])
     
+    var a: Store = Store(id: "30DNiNyclYIH5Bi5u5tX", name: "피자스쿨 정릉점", number: "02-919-0066", address: "서울 성북구 보국문로16길 31", category: "패스트푸드", menu: ["페페로니피자": 9900, "핫치킨피자": 10900, "고구마피자": 10900, "치즈피자": 8900, "콤비네이션피자": 10900], operatingHour: ["": ""], position: [37.0, 127.0])
+    
     var body: some View {
         ZStack {
-            Color("Symbol")
+//            Color("Symbol")
+//                .ignoresSafeArea()
+            Image("background_shop_detail")
+                .resizable()
                 .ignoresSafeArea()
+                .aspectRatio(contentMode: .fill)
             VStack {
                 ShopDetailHead()
                     .padding()
@@ -35,37 +41,57 @@ struct ShopDetailView: View {
                     switch model.category {
                     case "한식":
                         Text("🍲")
-                            .font(.system(size: 150))
-                            .offset(x: 100, y: -340)
+                            .font(.system(size: 180))
+                            .offset(x: 70, y: -370)
                     case "중식":
                         Text("🍛")
-                            .font(.system(size: 150))
-                            .offset(x: 100, y: -340)
+                            .font(.system(size: 180))
+                            .offset(x: 70, y: -370)
                     case "패스트푸드":
                         Text("🍔")
                             .font(.system(size: 150))
-                            .offset(x: 100, y: -340)
+                            .offset(x: 100, y: -370)
                     case "아시안":
                         Text("🍜")
-                            .font(.system(size: 150))
-                            .offset(x: 100, y: -340)
+                            .font(.system(size: 180))
+                            .offset(x: 90, y: -370)
                     case "분식":
                         Text("🥟")
                             .font(.system(size: 150))
-                            .offset(x: 100, y: -340)
+                            .offset(x: 70, y: -370)
                     case "카페":
                         Text("🍹")
-                            .font(.system(size: 150))
-                            .offset(x: 100, y: -340)
+                            .font(.system(size: 180))
+                            .offset(x: 70, y: -370)
                     default:
                         Text("")
                     }
                     ScrollView {
                         ForEach(model.menu.sorted(by: >), id: \.key) { key, value in
-                            Text(key)
-                            
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Spacer() //공백
+                                    Text(key)
+                                        .font(.system(size: 20))
+                                        .fontWeight(.semibold)
+                                    Spacer() //공백
+                                    Text(String(value))
+                                    Spacer() //공백
+                                }
+                                Spacer() //공백
+                                VStack {
+                                    Spacer()//공백
+                                    Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/) {
+                                        
+                                    }
+                                    Spacer()//공백
+                                }
+                            }
+                            .frame(height: 100)
+                            Divider()
                         }
                     }
+                    .padding()
                 }
                 .offset(y: 60)
             }
